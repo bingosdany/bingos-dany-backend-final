a28c67ad93099d157bafc48609300acedb52b03a
bingosdany <tartaraexpressalvajadas@gmail.com>
2025-06-04 13:22:39 -0500

    Agregada carpeta models con Reserva.js

--- models/Reserva.js
+++ models/Reserva.js
@@ -3,15 +3,9 @@ const mongoose = require('mongoose');
 const reservaSchema = new mongoose.Schema({
   nombre: String,
   correo: String,
-  cantidad: Number,
-  comprobante: String, // ✅ Comprobante subido (nombre del archivo)
   fecha: {
     type: Date,
     default: Date.now
-  },
-  validado: {
-    type: Boolean,
-    default: false // ✅ Campo nuevo para controlar si ya fue validado
   }
 });
 
